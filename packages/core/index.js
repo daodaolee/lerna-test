fc40052249286230bde64de8a95eee753e33297c
vlixin_v <vlixin_v@didiglobal.com>
2023-09-08 14:44:53 +0800

    chore: init

---
+++ packages/core/index.js
@@ -0,0 +1,2 @@
+const abbrev = require('abbrev')
+module.exports = abbrev("foo", "test")
---
+++ packages/shared/index.js
@@ -0,0 +1,6 @@
+function getDate(){
+  return Date.parse(new Date())
+}
+
+const moduleCore = require('core')
+console.log(' 🛻', '\n', '├──', 'moduleCore', '\n', '└──', moduleCore, '\n')
